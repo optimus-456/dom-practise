343f9620c15ca2305be37bfeaed968abbf21d4f0
Optimus <ijaolatoyosi03@gmail.com>
2024-07-24 10:29:33 +0100

    Added files and manipulated the dom

---
+++ script.js
@@ -0,0 +1,17 @@
+const addButton = document.getElementById("add-button");
+
+addButton.addEventListener("click", function addItem() {
+  const input = document.getElementById("input-text");
+  const inputValue = input.value.trim();
+
+  if (inputValue !== "") {
+    const list = document.createElement("li");
+    list.textContent = inputValue;
+
+    const listContainer = document.getElementById("list-container");
+
+    listContainer.appendChild(list);
+
+    input.value = "";
+  }
+});
